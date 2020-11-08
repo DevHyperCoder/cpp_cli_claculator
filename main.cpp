7a0bc91b9ca5dd16b4a7c662d9ea5715c1e932f1
Rachit <70371526+Rachithooda@users.noreply.github.com>
2020-11-08 21:45:03 +0530

    very basic c++ calculator

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main() {
+	int x;
+	int y;
+	int res;
+	char opr;
+	
+	std::cout << "Enter First Number: ";
+	std::cin >> x;
+	std::cout << "\n";
+	std::cout << "Enter Second Number: ";
+	std::cin >> y;
+	std::cout << "\n";
+	std::cout << "Enter Opreation (+, -, *, /): ";
+	std::cin >> opr;
+	std::cout << "\n";
+
+	while (true)
+	{
+		if (opr == '+')
+	{
+		res = x + y;
+		std::cout << res;
+		break;
+	}
+	else if (opr == '-')
+	{
+		res = x - y;
+		std::cout << res;
+		break;
+	}
+	else if (opr == '*')
+	{
+		res = x * y;
+		std::cout << res;
+		break;
+	}
+	else if (opr == '/')
+	{
+		res = x / y;
+		
+		break;
+	}
+	else {
+		std::cout << "Invaid Input";
+		continue;
+	}
+	}
+
+}
